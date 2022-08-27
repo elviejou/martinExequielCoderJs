00b3238f3e79ea1ac6c0da98ef8b4cbd2d35a6a7
elviejou <exequielcalo@gmail.com>
2022-08-27 14:10:51 -0300

    deafio1

--- desafio1.js
+++ desafio1.js
@@ -1,11 +1,8 @@
-
-
-
 alert("Bienvenidos al programa de Docentes por Materia. \n El mismo muestra el nombre de la materia, \n alumno con la nota mas alta")
 let materiaAlumnos = prompt("Por favor seleccione la materia para ingresar la nota: \n 1 - MATEMÁTICA \n 2 - HISTORIA \n 3 - FISICA")
 let cantidadAlumnos = prompt("Ingrese la cantidad de Alumnos que desea evaluar")
 let mayorNotaAlumno = 0
-let nombreAlumnoMayorNota:String 
+let nombreAlumnoMayorNota
 while (materiaAlumnos == "1" || materiaAlumnos == "2" || materiaAlumnos == 3 && cantidadAlumnos > "3"){
 
     for (let i = cantidadAlumnos; i > 0; i--){ 
@@ -18,16 +15,16 @@ while (materiaAlumnos == "1" || materiaAlumnos == "2" || materiaAlumnos == 3 && 
             }
             materiaAlumnos = "0"
     }
-    if (materiaAlumnos == "1"){
-            materiaAlumnos == "Matemática"
+    if (materiaAlumnos = 1){
+            materiaAlumnos = "Matemática"
         }
-        else if (materiaAlumnos == "2"){
-            materiaAlumnos == "Historia"
+        else if (materiaAlumnos = 2){
+            materiaAlumnos = "Historia"
         }
-        else if (materiaAlumnos == "3"){
-            materiaAlumnos == "Física"
+        else if (materiaAlumnos = 3){
+            materiaAlumnos = "Física"
         }
-    alert(`La materia elegida es ${mayorNotaAlumno} La nota mas alta es ${mayorNotaAlumno} y corresponde al alumno ${nombreAlumnoMayorNota}`)    
+    alert(`La materia elegida es ${materiaAlumnos} La nota mas alta es ${mayorNotaAlumno} y corresponde al alumno ${nombreAlumnoMayorNota}`)    
 }
 
 
