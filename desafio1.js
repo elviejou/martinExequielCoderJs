c6d6f8432a0721a40bd16dd90c44ddbd50411ad2
elviejou <exequielcalo@gmail.com>
2022-08-27 16:29:50 -0300

    Desafio1

--- desafio1.js
+++ desafio1.js
@@ -1,11 +1,11 @@
 alert("Bienvenidos al programa de Docentes por Materia. \n  \nEl mismo muestra el nombre de la materia, \nel nombre del alumno con la nota mas alta \ny la nota que sacó en el examen")
 let materiaAlumnos = prompt("Por favor seleccione la materia para ingresar la nota: \n 1 - MATEMÁTICA \n 2 - HISTORIA \n 3 - FISICA")
-let cantidadAlumnos = prompt("Ingrese la cantidad de Alumnos que desea evaluar")
 let mayorNotaAlumno = 0
 let nombreAlumnoMayorNota
 while (materiaAlumnos == "1" || materiaAlumnos == "2" || materiaAlumnos == "3"){
-
+    let cantidadAlumnos = prompt("Ingrese la cantidad de Alumnos que desea evaluar")
     for (let i = cantidadAlumnos; i > 0; i--){ 
+
         let nombreAlumno = prompt("Ingrese el nombre del alumno")
         let notaAlumno = prompt("Ingrese la nota del alumno")
 
@@ -18,17 +18,33 @@ while (materiaAlumnos == "1" || materiaAlumnos == "2" || materiaAlumnos == "3"){
                 nombreAlumnoMayorNota = nombreAlumno
             }
 
-            materiaAlumnos = "0"
+           
     }
-    if (materiaAlumnos = 1){
-            materiaAlumnos = "Matemática"
-        }
-        else if (materiaAlumnos = 2){
-            materiaAlumnos = "Historia"
-        }
-        else if (materiaAlumnos = 3){
-            materiaAlumnos = "Física"
-        }
+    // if (materiaAlumnos = 1){
+    //         materiaAlumnos = "Matemática"
+    //     }
+    //     else if (materiaAlumnos = 2){
+    //         materiaAlumnos = "Historia"
+    //     }
+    //     else if (materiaAlumnos = 3){
+    //         materiaAlumnos = "Física"
+    //     }
+
+        switch (materiaAlumnos) {
+            case "1":
+                materiaAlumnos = "Matemática"
+                break;
+            case "2":
+                materiaAlumnos = "Historia"
+                break;
+            case "3":
+                materiaAlumnos = "Física"
+              break;
+            default:
+                materiaAlumnos = "No especifico Materias"
+              break;
+          }
+
     alert(`La materia elegida es ${materiaAlumnos}. \n La nota mas alta es ${mayorNotaAlumno}. \n Corresponde al alumno ${nombreAlumnoMayorNota}`)    
 }
 
